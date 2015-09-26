fd68967cdef243c363025951bc69a3723e2c2850
blackyblack <blackyblack@mail.ru>
2015-09-27 02:52:14 +0500

    Work in progress

--- src/echodemo_server.erl
+++ src/echodemo_server.erl
@@ -2,14 +2,15 @@
 
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([start_link/0, process_req/2]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
 -record(state, {permanentflag, daemonid, ppid, name, numsent, numrecv, nxtaddr, servicenxtaddr, myid,
                 connectaddr, bindaddr, timeout, sleepmillis,
-                snetsocket, agentsocket, allowremote, registeredflag}).
+                snetsocket, agentsocket, allowremote, registeredflag,
+                alivetimer}).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
@@ -23,13 +24,11 @@ snetreply(Data, State) ->
   Socket = State#state.snetsocket,
   Numsent = State#state.numsent,
   ExtData = string:concat(Data, [0]),
-  Ret = enm:send(Socket, ExtData),
-  error_logger:info_msg("Send ret: ~p~n", [Ret]),
-  timer:sleep(1000),
+  ok = enm:send(Socket, ExtData),
   NewState = State#state{numsent = Numsent + 1},
   {ok, NewState}.
 
-main([PermArg, DaemonIdArg, JsonArgs, ParentIdArg]) ->
+main([MainPath, PermArg, DaemonIdArg, JsonArgs, ParentIdArg]) ->
   PermFlag = (PermArg == "1"),
   {DaemonId, _} = string:to_integer(DaemonIdArg),
   {ParentId, _} = string:to_integer(ParentIdArg),
@@ -37,10 +36,11 @@ main([PermArg, DaemonIdArg, JsonArgs, ParentIdArg]) ->
   error_logger:info_msg("JSON: ~p~n", [list_to_binary(JsonArgs)]),
   %% expect a stringified version of JSON
   {ok, [{string,_,JsonArgsParsed}],_}=erl_scan:string(JsonArgs),
-  error_logger:info_msg("JSONP: ~s~n", [list_to_binary(JsonArgsParsed)]),
   RandomBytes = procutils:randombytes(8),
   MyId = binary:decode_unsigned(RandomBytes),
   JsonData = jsx:decode(list_to_binary(JsonArgsParsed), [return_maps]),
+  SnAddress = lists:flatten(io_lib:format("ipc://~s/SuperNET.agents", [MainPath])),
+  ListenAddress = lists:flatten(io_lib:format("ipc://~s/~p", [MainPath, DaemonId])),
   error_logger:info_msg("Json: ~p~n", [JsonData]),
   {ok, #state{
     permanentflag = PermFlag,
@@ -49,11 +49,11 @@ main([PermArg, DaemonIdArg, JsonArgs, ParentIdArg]) ->
     name = "echodemo",
     numsent = 0,
     numrecv = 0,
-    nxtaddr=binary_to_integer(maps:get(<<"NXT">>, JsonData, <<"0">>)),
-    servicenxtaddr=binary_to_integer(maps:get(<<"serviceNXT">>, JsonData, <<"0">>)),
+    nxtaddr = binary_to_integer(maps:get(<<"NXT">>, JsonData, <<"0">>)),
+    servicenxtaddr = binary_to_integer(maps:get(<<"serviceNXT">>, JsonData, <<"0">>)),
     myid = MyId,
-    connectaddr = "ipc://SuperNET.agents",
-    bindaddr = string:concat("ipc://", integer_to_list(DaemonId)),
+    connectaddr = SnAddress,
+    bindaddr = ListenAddress,
     timeout = maps:get(<<"timeout">>, JsonData, 0),
     sleepmillis = maps:get(<<"sleepmillis">>, JsonData, 100),
     allowremote = 1,
@@ -66,8 +66,8 @@ init([]) ->
   error_logger:info_msg("State: ~p~n", [State]),
   {ok, Socket1} = enm:push([{connect, State#state.connectaddr}, list]),
   {ok, Socket2} = enm:bus([{bind, State#state.bindaddr}, {active, true}]),
-  error_logger:info_msg("Connected on ~p~n", [State#state.connectaddr]),
-  error_logger:info_msg("Listen on ~p~n", [State#state.bindaddr]),
+  error_logger:info_msg("Connected on ~s~n", [State#state.connectaddr]),
+  error_logger:info_msg("Listen on ~s~n", [State#state.bindaddr]),
 
   %% TODO: create register JSON and send to Push socket
   NewState = State#state{snetsocket = Socket1, agentsocket = Socket2},
@@ -83,6 +83,7 @@ init([]) ->
     <<"sent">> => State#state.numsent,
     <<"recv">> => State#state.numrecv,
     <<"NXT">> => list_to_binary(integer_to_list(State#state.nxtaddr)),
+    <<"serviceNXT">> => list_to_binary(integer_to_list(State#state.servicenxtaddr)),
     <<"endpoint">> => list_to_binary(State#state.bindaddr),
     <<"myid">> => list_to_binary(integer_to_list(State#state.myid)),
     <<"allowremote">> => State#state.allowremote,
@@ -90,8 +91,10 @@ init([]) ->
     <<"sleepmillis">> => State#state.sleepmillis
   },
   JsonStr = jsx:encode(RegData),
-  error_logger:info_msg("Register JSON ~p~n", [JsonStr]),
-  snetreply(binary_to_list(JsonStr), NewState).
+  {ok, NewState2} = snetreply(binary_to_list(JsonStr), NewState),
+  Timer = erlang:send_after(State#state.sleepmillis, self(), isalive, []),
+  NewState3 = NewState2#state{alivetimer = Timer},
+  {ok, NewState3}.
 
 handle_call(Request, From, State) ->
     error_logger:info_msg("echodemo call ~p from ~p~n", [Request, From]),
@@ -105,11 +108,72 @@ handle_info({_, {error, Msg}}, State) ->
     error_logger:info_msg("echodemo error: ~p. Stopping... ~n", [Msg]),
     {stop, {error, Msg}, State};
 
+handle_info(isalive, State) ->
+    erlang:cancel_timer(State#state.alivetimer),
+    Alive = procutils:os_pingpid(State#state.ppid),
+    if
+        Alive /= 0 ->
+	    error_logger:info_msg("Parent ~p died. Terminating.~n", [State#state.ppid]),
+            {stop, normal, State};
+        true ->
+            Timer = erlang:send_after(State#state.sleepmillis, self(), isalive, []),
+            NewState = State#state{alivetimer = Timer},
+            {noreply, NewState}
+    end;
+
+handle_info({nnbus, _Socket, DataRaw}, State) ->
+    RawSize = size(DataRaw),
+    Data = binary:part(DataRaw, 0, RawSize - 1),
+    error_logger:info_msg("nn_recv ~s~n", [binary_to_list(Data)]),
+    Json = jsx:decode(Data, [return_maps]),
+    error_logger:info_msg("json = ~p~n", [Json]),
+    PluginStr = maps:get(<<"plugin">>, Json, <<"">>),
+    AgentStr = maps:get(<<"agent">>, Json, <<"">>),
+    DestPluginStr = maps:get(<<"destplugin">>, Json, <<"">>),
+    DestAgentStr = maps:get(<<"destagent">>, Json, <<"">>),
+    if
+        (PluginStr == State#state.name) or (AgentStr == State#state.name) or
+        (DestPluginStr == State#state.name) or (DestAgentStr == State#state.name) ->
+            Answer = process_req(Json, State),
+            process_reply(Answer, Json);
+        true -> {noreply, State}
+    end;
+
 handle_info(Info, State) ->
     error_logger:info_msg("echodemo info ~p~n", [Info]),
     {noreply, State}.
 
-terminate(_Reason, _State) -> ok.
+terminate(_Reason, _State) -> init:stop(), ok.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+%% process good and bad responses from agent
+process_reply({ok, Data, State}, _Req) ->
+    JsonStr = jsx:encode(Data),
+    {ok, NewState} = snetreply(binary_to_list(JsonStr), State),
+    {noreply, NewState};
+
+process_reply({error, _Reason, State}, Req) ->
+    TagStr = maps:get(<<"tag">>, Req, nil),
+    Answer = #{
+        <<"result">> => <<"no response">>,
+        <<"tag">> => TagStr,
+        %% stdfields
+        <<"allowremote">> => State#state.allowremote,
+        <<"daemonid">> => list_to_binary(integer_to_list(State#state.daemonid)),
+        <<"NXT">> => list_to_binary(integer_to_list(State#state.nxtaddr)),
+        <<"serviceNXT">> => list_to_binary(integer_to_list(State#state.servicenxtaddr)),
+        <<"myid">> => list_to_binary(integer_to_list(State#state.myid))
+    },
+    JsonStr = jsx:encode(Answer),
+    {ok, NewState} = snetreply(binary_to_list(JsonStr), State),
+    {noreply, NewState}.
+
+%% move to specific agent implementation
+process_req(_Json, State) ->
+    error_logger:info_msg("processing agent request~n", []),
+    Reply = #{
+      <<"result">> => <<"ok">>
+    },
+    {ok, Reply, State}.
