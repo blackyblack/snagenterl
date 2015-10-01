8a78e555fde858e4745ea754581c26f873b4515c
blackyblack <blackyblack@mail.ru>
2015-10-02 01:19:37 +0500

    Agent plugin in progress

--- src/echodemo_server.erl
+++ src/echodemo_server.erl
@@ -2,7 +2,7 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, process_req/2]).
+-export([start_link/0]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
@@ -28,7 +28,7 @@ snetreply(Data, State) ->
   NewState = State#state{numsent = Numsent + 1},
   {ok, NewState}.
 
-main([MainPath, PermArg, DaemonIdArg, JsonArgs, ParentIdArg]) ->
+create_agent([MainPath, PermArg, DaemonIdArg, JsonArgs, ParentIdArg]) ->
   PermFlag = (PermArg == "1"),
   {DaemonId, _} = string:to_integer(DaemonIdArg),
   {ParentId, _} = string:to_integer(ParentIdArg),
@@ -42,11 +42,12 @@ main([MainPath, PermArg, DaemonIdArg, JsonArgs, ParentIdArg]) ->
   SnAddress = lists:flatten(io_lib:format("ipc://~s/SuperNET.agents", [MainPath])),
   ListenAddress = lists:flatten(io_lib:format("ipc://~s/~p", [MainPath, DaemonId])),
   error_logger:info_msg("Json: ~p~n", [JsonData]),
-  {ok, #state{
+
+   State = #state{
     permanentflag = PermFlag,
     daemonid = DaemonId,
     ppid = ParentId,
-    name = "echodemo",
+    name = agent_name(),
     numsent = 0,
     numrecv = 0,
     nxtaddr = binary_to_integer(maps:get(<<"NXT">>, JsonData, <<"0">>)),
@@ -57,28 +58,34 @@ main([MainPath, PermArg, DaemonIdArg, JsonArgs, ParentIdArg]) ->
     timeout = maps:get(<<"timeout">>, JsonData, 0),
     sleepmillis = maps:get(<<"sleepmillis">>, JsonData, 100),
     allowremote = 1,
-    registeredflag = 0}}.
+    registeredflag = 0},
+
+  agent_register(State, JsonData).
 
 %% gen_server callbacks
 
 init([]) ->
-  {ok, State} = main(application:get_env(echodemo, args, undefined)),
+  {ok, State, _DisabledMask} = create_agent(application:get_env(echodemo, args, undefined)),
   error_logger:info_msg("State: ~p~n", [State]),
   {ok, Socket1} = enm:push([{connect, State#state.connectaddr}, list]),
   {ok, Socket2} = enm:bus([{bind, State#state.bindaddr}, {active, true}]),
   error_logger:info_msg("Connected on ~s~n", [State#state.connectaddr]),
   error_logger:info_msg("Listen on ~s~n", [State#state.bindaddr]),
 
+  Methods = agent_methods(),
+  AuthMethods = agent_auth_methods(),
+  PubMethods = agent_pub_methods(),
+
   %% TODO: create register JSON and send to Push socket
   NewState = State#state{snetsocket = Socket1, agentsocket = Socket2},
   RegData = #{
     <<"daemonid">> => list_to_binary(integer_to_list(State#state.daemonid)),
-    <<"methods">> => [<<"echo">>],
-    <<"authmethods">> => [<<"echo">>],
-    <<"pubmethods">> => [<<"echo">>],
+    <<"methods">> => Methods,
+    <<"authmethods">> => AuthMethods,
+    <<"pubmethods">> => PubMethods,
     <<"pluginrequest">> => <<"SuperNET">>,
     <<"requestType">> => <<"register">>,
-    <<"plugin">> => list_to_binary(State#state.name),
+    <<"plugin">> => State#state.name,
     <<"permanentflag">> => State#state.permanentflag,
     <<"sent">> => State#state.numsent,
     <<"recv">> => State#state.numrecv,
@@ -116,50 +123,70 @@ handle_info(isalive, State) ->
 	    error_logger:info_msg("Parent ~p died. Terminating.~n", [State#state.ppid]),
             {stop, normal, State};
         true ->
-            Timer = erlang:send_after(State#state.sleepmillis, self(), isalive, []),
-            NewState = State#state{alivetimer = Timer},
-            {noreply, NewState}
+            {ok, NewState} = agent_idle(State),
+            Timer = erlang:send_after(NewState#state.sleepmillis, self(), isalive, []),
+            NewState2 = NewState#state{alivetimer = Timer},
+            {noreply, NewState2}
     end;
 
 handle_info({nnbus, _Socket, DataRaw}, State) ->
     RawSize = size(DataRaw),
     Data = binary:part(DataRaw, 0, RawSize - 1),
     error_logger:info_msg("nn_recv ~s~n", [binary_to_list(Data)]),
-    Json = jsx:decode(Data, [return_maps]),
-    error_logger:info_msg("json = ~p~n", [Json]),
-    PluginStr = maps:get(<<"plugin">>, Json, <<"">>),
-    AgentStr = maps:get(<<"agent">>, Json, <<"">>),
-    DestPluginStr = maps:get(<<"destplugin">>, Json, <<"">>),
-    DestAgentStr = maps:get(<<"destagent">>, Json, <<"">>),
-    if
-        (PluginStr == State#state.name) or (AgentStr == State#state.name) or
-        (DestPluginStr == State#state.name) or (DestAgentStr == State#state.name) ->
-            Answer = process_req(Json, State),
-            process_reply(Answer, Json);
-        true -> {noreply, State}
+    case jsx:is_json(Data) of
+        true ->
+            Json = jsx:decode(Data, [return_maps]),
+            error_logger:info_msg("json = ~p~n", [Json]),
+            {JsonReq, Token} = split_json_array(Json),
+            if
+                Token /= nil ->
+                    Forwarder = maps:get(<<"forwarder">>, Token, <<"">>),
+                    Sender = maps:get(<<"sender">>, Token, <<"">>),
+                    Valid = maps:get(<<"valid">>, Token, 0),
+                    error_logger:info_msg("token: forwarder = ~s, sender = ~s, valid = ~p~n", [Forwarder, Sender, Valid]);
+                true -> ok
+            end,
+            PluginStr = maps:get(<<"plugin">>, JsonReq, <<"">>),
+            AgentStr = maps:get(<<"agent">>, JsonReq, <<"">>),
+            DestPluginStr = maps:get(<<"destplugin">>, JsonReq, <<"">>),
+            DestAgentStr = maps:get(<<"destagent">>, JsonReq, <<"">>),
+            TagStr = maps:get(<<"tag">>, JsonReq, nil),
+            if
+                (PluginStr == State#state.name) or (AgentStr == State#state.name) or
+                (DestPluginStr == State#state.name) or (DestAgentStr == State#state.name) ->
+                    Answer = agent_process_req(JsonReq, State),
+                    process_reply(Answer, JsonReq, TagStr);
+                true -> {noreply, State}
+            end;
+        false ->
+            error_logger:info_msg("couldnt parse (~s)~n", [binary_to_list(Data)]),
+            Answer = #{
+                <<"result">> => <<"unparseable">>,
+                <<"message">> => Data
+            },
+            {ok, NewState} = reply_with_stdfields(Answer, State),
+            {noreply, NewState}
     end;
 
 handle_info(Info, State) ->
     error_logger:info_msg("echodemo info ~p~n", [Info]),
     {noreply, State}.
 
-terminate(_Reason, _State) -> init:stop(), ok.
+terminate(_Reason, State) -> agent_shutdown(State, 0), init:stop(), ok.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-%% process good and bad responses from agent
-process_reply({ok, Data, State}, _Req) ->
-    JsonStr = jsx:encode(Data),
-    {ok, NewState} = snetreply(binary_to_list(JsonStr), State),
-    {noreply, NewState};
+%% Utility
 
-process_reply({error, _Reason, State}, Req) ->
-    TagStr = maps:get(<<"tag">>, Req, nil),
-    Answer = #{
-        <<"result">> => <<"no response">>,
-        <<"tag">> => TagStr,
-        %% stdfields
+split_json_array([Json, Rest]) ->
+    {Json, Rest};
+
+split_json_array(Json) ->
+    {Json, nil}.
+
+reply_with_stdfields(Reply, State) ->
+    Answer = Reply#{
         <<"allowremote">> => State#state.allowremote,
         <<"daemonid">> => list_to_binary(integer_to_list(State#state.daemonid)),
         <<"NXT">> => list_to_binary(integer_to_list(State#state.nxtaddr)),
@@ -167,13 +194,94 @@ process_reply({error, _Reason, State}, Req) ->
         <<"myid">> => list_to_binary(integer_to_list(State#state.myid))
     },
     JsonStr = jsx:encode(Answer),
-    {ok, NewState} = snetreply(binary_to_list(JsonStr), State),
+    snetreply(binary_to_list(JsonStr), State).
+
+%% process good and bad responses from agent
+process_reply({ok, Data, State}, _Req, nil) ->
+    {ok, NewState} = reply_with_stdfields(Data, State),
+    {noreply, NewState};
+
+process_reply({ok, Data, State}, _Req, Tag) ->
+    Answer = Data#{
+        <<"tag">> => Tag
+    },
+    {ok, NewState} = reply_with_stdfields(Answer, State),
+    {noreply, NewState};
+
+process_reply({error, _Reason, State}, _Req, nil) ->
+    Answer = #{
+        <<"result">> => <<"no response">>
+    },
+    {ok, NewState} = reply_with_stdfields(Answer, State),
+    {noreply, NewState};
+
+process_reply({error, _Reason, State}, _Req, Tag) ->
+    Answer = #{
+        <<"result">> => <<"no response">>,
+        <<"tag">> => Tag
+    },
+    {ok, NewState} = reply_with_stdfields(Answer, State),
     {noreply, NewState}.
 
 %% move to specific agent implementation
-process_req(_Json, State) ->
-    error_logger:info_msg("processing agent request~n", []),
-    Reply = #{
-      <<"result">> => <<"ok">>
+process_json_req(_Error, <<"registered">>, _Method, _Req, State) ->
+    Answer = #{
+        <<"result">> => <<"activated">>
     },
-    {ok, Reply, State}.
+    {ok, Answer, State};
+
+process_json_req(Error, _Result, _Method, _Req, State) when Error /= nil ->
+    Answer = #{
+        <<"result">> => <<"completed">>
+    },
+    {ok, Answer, State};
+
+process_json_req(_Error, Result, _Method, _Req, State) when Result /= nil ->
+    Answer = #{
+        <<"result">> => <<"completed">>
+    },
+    {ok, Answer, State};
+
+process_json_req(_Error, _Result, nil, Req, State) ->
+    error_logger:info_msg("request (~p) has not method~n", [Req]),
+    {error, nil, State};
+
+process_json_req(_Error, _Result, <<"echo">>, Req, State) ->
+    Message = maps:get(<<"echostr">>, Req, <<"">>),
+    Answer = #{
+        <<"result">> => Message
+    },
+    {ok, Answer, State};
+
+%% no response from agent
+process_json_req(_Error, _Result, _Method, _Req, State) ->
+    {error, nil, State}.
+
+%% agent API
+
+agent_name() -> <<"echodemo">>.
+agent_methods() -> [<<"echo">>].
+agent_auth_methods() -> [<<"echo">>].
+agent_pub_methods() -> [<<"echo">>].
+
+%% register from JSON object
+%% return {ok, State, DisabledMethodsMask}
+agent_register(State, _Args) -> {ok, State, 0}.
+
+%% initialize from JSON object. process JSON, return JSON
+%agent_process_register(State, _Args) ->
+%    NewState = State#state{allowremote = 1},
+%    Answer = #{
+%        <<"result">> => <<"echodemo init">>
+%    },
+%    {ok, Answer, NewState}.
+
+agent_idle(State) -> {ok, State}.
+agent_shutdown(State, _Reason) -> {ok, State}.
+
+agent_process_req(Json, State) ->
+    error_logger:info_msg("processing agent request~n", []),
+    Result = maps:get(<<"result">>, Json, nil),
+    Error = maps:get(<<"error">>, Json, nil),
+    Method = maps:get(<<"method">>, Json, nil),
+    process_json_req(Error, Result, Method, Json, State).
